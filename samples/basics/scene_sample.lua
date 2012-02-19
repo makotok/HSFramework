447a7375ca44cbd7bba5de3d5533463c5f91080d
makoto <makotok1123@gmail.com>
2012-02-20 02:11:58 +0900

    sample update

--- samples/basics/scene_sample.lua
+++ samples/basics/scene_sample.lua
@@ -10,8 +10,14 @@ local group = Group:new({layout = VBoxLayout:new(), parent = scene})
 local sprite1 = Sprite:new("samples/images/back_1.png", {parent = group})
 local sprite2 = Sprite:new("samples/images/cathead.png", {x = 10, y = 10, parent = group})
 
-Animation:new(scene, 1):fadeOut():fadeIn():parallel(
+--[[
+Animation:new(scene, 1):fadeOut():fadeIn():move(240, 0):parallel(
     Animation:new(scene.topLayer.camera, 1):rotate(360)
+):play()
+--]]
+Animation:new(scene, 1):copy({x = -25, y = 0}):parallel(
+    Animation:new(scene, 1):move(25, 0),
+    Animation:new(scene, 1):fadeIn()
 ):play()
 
 scene:openScene()
