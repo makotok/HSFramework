c1ee6366ae38836cb29910f0d177252df1f62c36
makoto <makotok1123@gmail.com>
2012-02-27 01:30:37 +0900

    updated:main

--- main.lua
+++ main.lua
@@ -2,20 +2,5 @@
 -- requires
 require "hs/HSFramework"
 
+-- main
 require "samples/sample_main"
-
--- basic samples
---require "samples/basics/sprite_sample"
---require "samples/basics/mapsprite_sample"
---require "samples/basics/group_sample"
---require "samples/basics/layer_sample"
---require "samples/basics/scene_sample"
---require "samples/basics/textlabel_sample"
-
--- animation samples
---require "samples/animations/anime_basic_sample"
---require "samples/animations/anime_fade_sample"
---require "samples/animations/anime_group_sample"
-
--- utils samples
---require "samples/utils/fps_sample"
